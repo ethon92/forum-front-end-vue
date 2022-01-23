8a8faa136004da62d00b1190a8cfa182e87a98b3
ethon92 <e19920526@gmail.com>
2022-01-23 14:42:03 +0800

    feat: add NotFound component

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import NotFound from '../views/NotFound.vue'
 
 Vue.use(VueRouter)
 
@@ -17,6 +18,13 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  // 由於路由是由上往下匹配，所以只要上方都匹配不到路由就會進到not-found
+  // 則將not-found路由放在最後一個，並且path是放入"*"(萬用字元)
+  {
+    path: '*',
+    name: 'not-found',
+    component: NotFound
   }
 ]
 
