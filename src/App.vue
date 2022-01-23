326b736b5e8e24e9a303a6468e99850f36cde7c2
ethon92 <e19920526@gmail.com>
2022-01-23 14:52:42 +0800

    feat: import bootstrap

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,19 @@
   </div>
 </template>
 
+<script>
+// 載入bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css'
+// 載入bootstrap javascript，由於在bootstrap的package.json中的main，
+// 已經寫好直接載入bootstrap時要執行哪一個程式，所以可以直接載入即可
+import 'bootstrap'
+
+export default {
+  name: 'App'
+}
+</script>
+
+
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
