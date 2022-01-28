3c3fdbe84d92c91acad445c84dfdcb758de68c49
ethon92 <e19920526@gmail.com>
2022-01-28 21:11:47 +0800

    feat: modify RestaurantsTop

--- src/views/RestaurantsTop.vue
+++ src/views/RestaurantsTop.vue
@@ -6,15 +6,60 @@
     </h1>
     <hr>
 
-    <RestaurantsTopCard v-for="restaurant in restaurants" :key="restaurant.id" :initial-restaurant="restaurant"/>
+    <div
+      v-for="restaurant in restaurants" :key="restaurant.id"
+      class="card mb-3"
+      style="max-width: 540px;margin: auto;"
+    >
+      <div class="row no-gutters">
+        <div class="col-md-4">
+          <a href="#">
+            <img
+              class="card-img"
+              :src="restaurant.image"
+            >
+          </a>
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">
+              Mrs. Mckenzie Johnston
+            </h5>
+            <span class="badge badge-secondary">收藏數： {{ restaurant.FavoriteCount }}</span>
+            <p class="card-text">
+              {{ restaurant.description }}
+            </p>
+            <router-link
+              :to="{ name: 'restaurant', params: {id: restaurant.id}}"
+              class="btn btn-primary mr-2"
+            >Show</router-link>
+
+            <button
+              v-if="restaurant.isFavorited"
+              @click.prevent.stop="deleteFavorite(restaurant.id)"
+              type="button"
+              class="btn btn-danger mr-2"
+            >
+              移除最愛
+            </button>
+            <button
+              v-else
+              @click.prevent.stop="addFavorite(restaurant.id)"
+              type="button"
+              class="btn btn-primary"
+            >
+              加到最愛
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // 載入NavTabs component
 import NavTabs from '../components/NavTabs.vue'
-// 載入RestaurantsTopCard component
-import RestaurantsTopCard from '../components/RestaurantsTopCard.vue'
 
 const dummyData = {
     "restaurants": [
@@ -186,7 +231,7 @@ export default {
   name: 'RestaurantsTop',
   components: {
     NavTabs,
-    RestaurantsTopCard
+    // RestaurantsTopCard
   },
   data() {
     return {
@@ -203,6 +248,12 @@ export default {
 
       this.restaurants = dummyData.restaurants
       // console.log(this.restaurants)
+    },
+    addFavorite(id) {
+      this.restaurants.filter( restaurant => restaurant.id === id )[0].isFavorited = true
+    },
+    deleteFavorite(id) {
+      this.restaurants.filter( restaurant => restaurant.id === id )[0].isFavorited = false
     }
   }
 }
