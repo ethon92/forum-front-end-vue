b57b6f04ee776a2caed2275a8d40045830ba4f91
ethon92 <e19920526@gmail.com>
2022-02-17 20:54:51 +0800

    feat: modify User page to connect with API

--- src/components/UserProfileCard.vue
+++ src/components/UserProfileCard.vue
@@ -2,28 +2,59 @@
   <div class="card mb-3">
     <div class="row no-gutters">
       <div class="col-md-4">
-          <img :src="userProfile.image | emptyImage" width="300px" height="300px">
+        <img
+          :src="user.image | emptyImage"
+          width="300px"
+          height="300px"
+        >
       </div>
       <div class="col-md-8">
         <div class="card-body">
-          <h5 class="card-title">{{ userProfile.name }}</h5>
+          <h5 class="card-title">
+            {{ user.name }}
+          </h5>
           <p class="card-text">
-            {{ userProfile.email }}
+            {{ user.email }}
           </p>
-          <ul class="list-unstyled list-inline">
-            <li><strong>{{ userProfile.commentedRestaurantsCount }}</strong> 已評論餐廳</li>
-            <li><strong>{{ userProfile.favoritedRestaurantsCount }}</strong> 收藏的餐廳</li>
-            <li><strong>{{ userProfile.followings }}</strong> followings (追蹤者)</li>
-            <li><strong>{{ userProfile.followers }}</strong> followers (追隨者)</li>
+          <ul class="list-unstyled">
+            <li>
+              <strong>{{ user.commentsLength }}</strong> 已評論餐廳
+            </li>
+            <li>
+              <strong>{{ user.favoritedRestaurantsLength }}</strong> 收藏的餐廳
+            </li>
+            <li>
+              <strong>{{ user.followingsLength }}</strong> followings (追蹤者)
+            </li>
+            <li>
+              <strong>{{ user.followersLength }}</strong> followers (追隨者)
+            </li>
           </ul>
-          <template v-if="userProfile.user.isAuthenticated">
-            <router-link :to="{ name: 'user-edit', params: { id: userProfile.user.id }}">
-              <button type="submit" class="btn btn-success">Edit</button>
+          <template v-if="isCurrentUser">
+            <router-link
+              :to="{ name: 'user-edit', params: { id: user.id } }"
+              class="btn btn-primary"
+            >
+              Edit
             </router-link>
           </template>
           <template v-else>
-            <button v-if="userProfile.isFollowed" @click="switchFollowedValue(false)" type="submit" class="btn btn-danger">取消追蹤</button>
-            <button v-else type="submit" @click="switchFollowedValue(true)" class="btn btn-primary">追蹤</button>
+            <button
+              v-if="isFollowed"
+              type="button"
+              class="btn btn-danger"
+              @click.stop.prevent="deleteFollowing(user.id)"
+            >
+              取消追蹤
+            </button>
+            <button
+              v-else
+              type="button"
+              class="btn btn-primary"
+              @click.stop.prevent="addFollowing(user.id)"
+            >
+              追蹤
+            </button>
           </template>
         </div>
       </div>
@@ -31,25 +62,67 @@
   </div>
 </template>
 
-<script>// 從mixins載入emptyImageFilter
-import { emptyImageFilter } from '../utils/mixins'
+<script>
+import { emptyImageFilter } from './../utils/mixins'
+import usersAPI from './../apis/users'
+import { Toast } from './../utils/helpers'
 
 export default {
   mixins: [emptyImageFilter],
   props: {
-    initialUserProfile: {
+    user: {
       type: Object,
+      required: true
+    },
+    isCurrentUser: {
+      type: Boolean,
+      required: true
+    },
+    initialIsFollowed: {
+      type: Boolean,
       required: true
     }
   },
-  data() {
-    return  {
-      userProfile: this.initialUserProfile
+  data () {
+    return {
+      isFollowed: this.initialIsFollowed
+    }
+  },
+  watch: {
+    initialIsFollowed (isFollowed) {
+      this.isFollowed = isFollowed
     }
   },
   methods: {
-    switchFollowedValue(value) {
-      return this.userProfile.isFollowed = value
+    async addFollowing (userId) {
+      try {
+        const { data } = await usersAPI.addFollowing({ userId })
+        if (data.status === 'error') {
+          throw new Error(data.message)
+        }
+        this.isFollowed = true
+      } catch (error) {
+        console.error(error.message)
+        Toast.fire({
+          icon: 'error',
+          title: '無法加入追蹤，請稍後再試'
+        })
+      }
+    },
+    async deleteFollowing (userId) {
+      try {
+        const { data } = await usersAPI.deleteFollowing({ userId })
+        if (data.status === 'error') {
+          throw new Error(data.message)
+        }
+        this.isFollowed = false
+      } catch (error) {
+        console.error(error.message)
+        Toast.fire({
+          icon: 'error',
+          title: '無法取消追蹤，請稍後再試'
+        })
+      }
     }
   }
 }
